a2e38f3b3d6dd20ec5d8ff70405b548afaa4510b
Fiala <zack.seed.4044.@gmail.com>
2024-04-04 00:20:37 +0100

    second commit on raouf branch

--- src/Components/Dashboard/Body Section/Top Section/Top.jsx
+++ src/Components/Dashboard/Body Section/Top Section/Top.jsx
@@ -2,39 +2,45 @@ import {BiSearchAlt} from 'react-icons/bi'
 import {TbMessageCircle} from 'react-icons/tb'
 import {IoNotificationsOutline} from 'react-icons/io5'
 import { BsArrowRightShort } from 'react-icons/bs';
-import DropDownProfile from '@/Components/DropDownProfile';
-import { useState } from 'react';
 
 const Top = () => {
-    const [openProfile, setOpenProfile] = useState(false);
     return ( 
-        
         <div className="topSection">
             <div className="headerSection flex">
-                <div className="title">
-                    <h1>Welcome to (Website).</h1>
-                    <p>Hello Zack, Welcome Back!</p>
-                </div>
+    <div className="adminDiv flex">
+    <button class="button">
+   <svg viewBox="0 0 448 512" class="bell"><path d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"></path></svg>
+</button>
+<button class="button" aria-label="Chat">
+    <svg viewBox="0 0 512 512" class="chat-icon" aria-hidden="true">
+        <path fill="currentColor" d="M460 0H52C23.3 0 0 23.3 0 52v364c0 28.7 23.3 52 52 52h116.7l-10.7 64L256 468.7l97.3 11.3L399.3 468l-10.7-64H460c28.7 0 52-23.3 52-52V52c0-28.7-23.3-52-52-52zM255.7 384l-11.7 64-45.3-38.7L64 448h-12c-9.3 0-16-7.3-16-16V80c0-8.7 6.7-16 16-16h384c9.3 0 16 7.3 16 16v352c0 9.3-6.7 16-16 16h-12l-135.3-99.7-45.3 38.7-11.7-64z"></path>
+    </svg>
+</button>
+<button class="button" aria-label="Add">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="add-icon" aria-hidden="true">
+        <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"></path>
+    </svg>
+</button>
 
-                <div className="searchBar flex">
-                    <input type="text" placeholder="Search Dashboard" />
-                    <BiSearchAlt className="icon"/>
-                </div>
 
-                <div className="adminDiv flex">
-                    <TbMessageCircle className="icon"/>
-                    <IoNotificationsOutline className="icon"/>
-                    <div className="adminImage" onClick={() => setOpenProfile((prev) => !prev)}>
-                        <img src="/loginAssets/user.png" alt="Admin Image" />
-                        
-                    </div>
-                    {/* {
-                            openProfile && <DropDownProfile />
-                    } */}
-                    
-                </div>
 
-            </div>
+
+
+
+    </div>
+    <div className="searchBar flex">
+    <input type="text" placeholder="Search Dashboard" className="searchInput" />
+    <BiSearchAlt className="icon"/>
+</div>
+
+    <div className="title">
+        <h1>Welcome to (Website).</h1>
+        <p>Hello Zack, Welcome Back!</p>
+    </div>
+
+    
+</div>
+
 
             <div className="cardSection flex">
                 <div className="rightCard flex">
@@ -72,7 +78,16 @@ const Top = () => {
                             <img src="/loginAssets/books.png" alt="Image Name" />
                         </div>
 
-                        
+                        {/* <div className="sideBarCard">
+                <BsQuestionCircle className='icon'/>
+                <div className="cardContent">
+                    <div className="circle1"></div>
+                    <div className="circle2"></div>
+                    <h3>Help Center</h3>
+                    <p>Having trou  ble in (website), please contact us for more questions.</p>
+                    <button className='btn'>Go to help center</button>
+                </div>
+            </div> */}
                     </div>
 
                 </div>
--- src/Components/Dashboard/SideBar Section/SideBar.jsx
+++ src/Components/Dashboard/SideBar Section/SideBar.jsx
@@ -1,5 +1,10 @@
 import styles from '@/styles/SideBar.module.css'
 import Link from 'next/link';
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdOutlineSecurity } from "react-icons/md";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { MdOutlineSaveAlt } from "react-icons/md";
 
 import { IoMdSpeedometer } from 'react-icons/io'
 import { RiKakaoTalkFill } from 'react-icons/ri'
@@ -8,100 +13,63 @@ import { BsQuestionCircle, BsTrophy } from 'react-icons/bs'
 
 
 const Sidebar = () => {
-
-    
     return ( 
         <div className='sideBar grid' id={styles.sideBar}>
 
 
-            <div className={styles.logoDiv}>
-                <img src="/loginAssets/logo.png" className={styles.logo} />
-                <h2>Website.</h2>
-            </div>
 
-            <div className="menuDiv">
-                <h3 className="divTitle">
-                    QUICK MENU
-                </h3>
-                <ul className="menuLists grid">
 
-                    <li className="listItems">
-                        <Link href="#" className='menuLink flex'>
-                            <IoMdSpeedometer className="icon"/>
-                            <span className="smallText">
-                                Dashboard
-                            </span>
-                        </Link>
-                    </li>
 
-                    <li className="listItems">
-                        <Link href="#" className='menuLink flex'>
-                            <RiKakaoTalkFill className="icon"/>
-                            <span className="smallText">
-                                My courses
-                            </span>
-                        </Link>
-                    </li>
-
-                    <li className="listItems">
-                        <Link href="#" className='menuLink flex'>
-                            <MdOutlineExplore className="icon"/>
-                            <span className="smallText">
-                                Explore
-                            </span>
-                        </Link>
-                    </li>
-
-                    <li className="listItems">
-                        <Link href="#" className='menuLink flex'>
-                            <BsTrophy className="icon"/>
-                            <span className="smallText">
-                                Something
-                            </span>
-                        </Link>
-                    </li>
-
-                </ul>
-            </div>
+<div className={styles.profileDiv}>
+    <div className={styles.profileImage}>
+        <img src="loginAssets\user.png" alt="Profile Image" />
+    </div>
+{/* add link t3 profile page */}
+        <a className={styles.profileText}>My Profile</a>
+    
+</div>
+ 
+          
             
             <div className="settingsDiv">
                 <h3 className="divTitle">
-                    Settings
+                <IoSettingsOutline className="icon"/>
+                        Settings
                 </h3>
                 <ul className="menuLists grid">
 
                     <li className="listItems">
                         <Link href="#" className='menuLink flex'>
-                            <IoMdSpeedometer className="icon"/>
+                            <FaRegUserCircle  className="icon"/>
                             <span className="smallText">
-                                Dashboard
+                                My Account
                             </span>
                         </Link>
                     </li>
 
                     <li className="listItems">
                         <Link href="#" className='menuLink flex'>
-                            <RiKakaoTalkFill className="icon"/>
+                            <MdOutlineSecurity className="icon"/>
                             <span className="smallText">
-                                My courses
+                                Security
                             </span>
                         </Link>
                     </li>
 
                     <li className="listItems">
                         <Link href="#" className='menuLink flex'>
-                            <MdOutlineExplore className="icon"/>
+                            <IoMdNotificationsOutline className="icon"/>
                             <span className="smallText">
-                                Explore
+                                Notification
                             </span>
                         </Link>
                     </li>
 
                     <li className="listItems">
                         <Link href="#" className='menuLink flex'>
-                            <BsTrophy className="icon"/>
+                            <MdOutlineSaveAlt className="icon"/>
                             <span className="smallText">
-                                Something
+                                 Saved
                             </span>
                         </Link>
                     </li>
@@ -115,7 +83,7 @@ const Sidebar = () => {
                     <div className="circle1"></div>
                     <div className="circle2"></div>
                     <h3>Help Center</h3>
-                    <p>Having trouble in (website), please contact us for more questions.</p>
+                    <p>Having trou  ble in (website), please contact us for more questions.</p>
                     <button className='btn'>Go to help center</button>
                 </div>
             </div>
